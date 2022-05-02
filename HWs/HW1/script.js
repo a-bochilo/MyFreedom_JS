3622716b9cac53d9d41e6526af73604e993cced1
Alexander Bochilo <a.bochilo@mail.ru>
2022-05-02 16:58:40 +0300

    js.hw1

---
+++ HWs/HW1/script.js
@@ -0,0 +1,73 @@
+"use strict";
+// Task 1
+alert("Привет, это работа Бочило Александра");
+
+// Task 2
+const OUR_PLANET_NAME = "Earth";
+let adminAge = 33;
+let brokenVehicle = "VAZ";
+let headerColor = "#F00";
+let isAdmin = false;
+console.log(
+  "Task 2 result: ",
+  OUR_PLANET_NAME,
+  adminAge,
+  brokenVehicle,
+  headerColor,
+  isAdmin
+);
+
+// Task 3
+let adminName = "Александр Бочило";
+console.log(
+  "Task 3 result: ",
+  `Привет, меня зовут ${adminName}, мне ${adminAge} года`
+);
+
+// Task 4
+let isInspector = confirm("Вы проверяете мою работу?");
+let inspectorName;
+if (isInspector === true)
+  (inspectorName = prompt("Как Вас зовут?")),
+    console.log("Task 4 result: ", `Мою работу проверяет ${inspectorName}`);
+else console.log("Task 4 result: ", "Not inspector");
+
+// Task 5
+let str = "String",
+  num = 2000,
+  testSkiped = false,
+  month = undefined,
+  data = null;
+console.log(
+  "Task 5 result: ",
+  typeof str,
+  typeof num,
+  typeof testSkiped,
+  typeof month,
+  typeof data
+);
+
+// Task 6
+str = "37";
+str = Number(str);
+console.log("Task 6, result 1.1: ", typeof str, str);
+str = Boolean(str);
+console.log("Task 6, result 1.2: ", typeof str, str);
+
+let value = true;
+value = String(value);
+console.log("Task 6, result 2.1: ", typeof value, value);
+value = Number(value);
+console.log("Task 6, result 2.2: ", typeof value, value);
+
+num = 2007;
+num = String(num);
+console.log("Task 6, result 1.1: ", typeof num, num);
+num = Boolean(num);
+console.log("Task 6, result 1.2: ", typeof num, num);
+
+// Task 7
+let name = "Ivan",
+  surname;
+surname = name;
+console.log("Task 7 result:", surname);
--- Lesson1/js/script.js
+++ Lesson1/js/script.js
@@ -1,7 +1,46 @@
-console.log("Hello");
+// console.log("Hello");
 
-let message = "Our message";
+// let xmessage = "Our message";
 
-console.log(message);
+// console.log(message);
 
-alert(message);
+// alert(message);
+
+// let name = "Ivan";
+// let surname = "Ivanov";
+// surname = name;
+// console.log(name);
+// console.log(name, surname);
+
+// let str = "Привет";
+// let str2 = "Одинарные кавычки тоже подойдут";
+// let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+// let phrase2 = `Обратные кавычки позволяют встраивать переменные ${str2}`;
+// let number = 235;
+// let number2 = "223";
+// console.log(phrase, phrase2, number, number2);
+
+// let k;
+// console.log(k);
+
+// console.log(typeof str);
+
+// let userName = prompt("Enter your name");
+// console.log(userName, typeof userName);
+
+// let isCorrecName = confirm("Is your name correct?");
+// console.log(typeof isCorrecName, isCorrecName);
+
+// let x = true;
+// console.log(typeof x);
+// let y = String(x);
+// console.log(y, typeof y);
+
+let str = "123 5";
+console.log(typeof str);
+let num = Number(str);
+console.log(typeof num, num);
+
+let check = "string";
+let check2 = check / 2;
+console.log(check2, typeof check2);
