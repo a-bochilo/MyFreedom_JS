fdb52c06f9912b9eb9d001351eb95938d907afe6
Alexander Bochilo <a.bochilo@mail.ru>
2022-05-05 09:25:58 +0300

    js.lesson2

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,12 +1,21 @@
 console.log("Text");
 
-let a = 3;
-if (a > 3) {
-  console.log("higher");
-} else if (a === 3) {
-  console.log("equals");
-} else {
-  console.log("lower");
-}
+// let a = 3;
+// if (a > 3) {
+//   console.log("higher");
+// } else if (a === 3) {
+//   console.log("equals");
+// } else {
+//   console.log("lower");
+// }
 
+// let a = 0;
+// let result = a > 0 ? "more" : "less";
+// console.log(result);
+
+// let a = 4;
+// let result = a > 5 ? "more" : a === 5 ? "equals" : "less";
+// console.log(result);
+
+let result = (a + b < 4) ? 'Less' : 'Too much';
 
