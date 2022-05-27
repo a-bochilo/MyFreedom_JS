f2a573a60137e1f49eee57640955dc1aaf57cb34
Alexander Bochilo <a.bochilo@mail.ru>
2022-05-27 21:47:49 +0300

    js.lesson 9

---
+++ Lesson8/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// numbers = numbers.map((item) => item ** 2);
+// console.log(numbers);
+
+// let value = arr.reduce(function(previousValue, item, index, array);
+
+// let sum = numbers.reduce((acc, item) => {
+//   return (acc += item);
+// }, 0); // 0 - start value of acc
+// console.log(sum);
+
+// 1st way:
+// let numbers = [1, 2, 1, -2, 3, 9, 11, -20, -5, -7, 10];
+// let sum = 1;
+// numbers.reduce((acc, item) => {
+//   if (acc < 10) sum++;
+//   return (acc += item);
+// });
+// console.log(sum);
+
+//2nd way
+// let numbers = [1, 2, 1, -2, 3, 9, 11, -15, 10, 5];
+// let sum = 1;
+// let sum2 = 0;
+// debugger;
+// numbers.reduce((acc, item) => {
+//   if (acc > 10) {
+//     sum2 = sum;
+//     // console.log(sum);
+//     return;
+//   }
+//   sum++;
+//   return (acc += item);
+// });
+// console.log(sum2);
