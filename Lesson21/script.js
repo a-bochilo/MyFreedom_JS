1089573f7e2a0ee6c8fcc491c85c3c1af76c9275
Alexander Bochilo <a.bochilo@mail.ru>
2022-07-11 09:23:29 +0300

    js.lesson 21

--- Lesson21/script.js
+++ Lesson21/script.js
@@ -1,22 +1,47 @@
 "use strict";
 
-function onChange(event) {
-  console.log(event.target.value);
-}
+// function onChange(event) {
+//   console.log(event.target.value);
+// }
 
-document.getElementById("search").addEventListener("keyup", onChange);
+// document.getElementById("search").addEventListener("keyup", onChange);
 
-const dobounce = (fn, ms) => {
-  let timeoutId;
-  return function () {
-    fnCall = () => {
-      fn.apply(this, arguments);
-    };
+// const dobounce = (fn, ms) => {
+//   let timeoutId;
+//   return function () {
+//     fnCall = () => {
+//       fn.apply(this, arguments);
+//     };
 
-    clearTimeout(timeoutId);
+//     clearTimeout(timeoutId);
 
-    timeoutId = setTimeout(fnCall, ms);
-  };
+//     timeoutId = setTimeout(fnCall, ms);
+//   };
+// };
+
+// onChange = dobounce(onChange, 1000);
+
+//
+//
+// const number = 200;
+// const material = "Block";
+
+// localStorage.setItem("number", number);
+
+// localStorage.clear();
+
+// console.log(localStorage.getItem("number"));
+
+//
+//
+const car = {
+  color: "green",
+  model: "Kia",
+  price: 44000,
 };
 
-onChange = dobounce(onChange, 1000);
+localStorage.setItem("car", car);
+
+const auto = localStorage.getItem("car");
+
+window.addEventListener("storage", (event) => console.log(event));
